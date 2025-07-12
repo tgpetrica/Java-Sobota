82a8764696ab191e99746f1fbadc0de4d234f8b0
Teodor <teodor.petrica@rovacc.ro>
2025-07-12 14:09:31 +0300

    add L26

---
+++ PregatireZbor.java
@@ -0,0 +1,131 @@
+// verificarea combustibilului incarcat la bord
+// pilotii sunt la bord
+// usile avionului sunt inchise
+// mtow check
+// sistemele avionului (AP, hidraulic, motoare, etc) sunt functionale
+// verificarea conditiilor meteorologice
+// presiunea din pneuri
+// depunerea planului de zbor
+// autorizatia ATC
+
+import java.lang.reflect.Executable;
+
+class FuelException extends Exception
+{
+    private final int fuelLevel;
+
+    public FuelException(int fuelLevel)
+    {
+        super ("Combustibil insuficient: " + fuelLevel + "% (minim 30%).");
+        this.fuelLevel = fuelLevel;
+    }
+
+    public int getFuelLevel()
+    {
+        return fuelLevel;
+    }
+}
+
+class PilotAbsenceException extends Exception
+{
+    public PilotAbsenceException()
+    {
+        super("Pilotii nu sunt prezenti in carlinga.");
+    }
+}
+
+class DoorsOpenException extends Exception
+{
+    public DoorsOpenException()
+    {
+        super ("Una sau mai multe usi ale avionului sunt deschise.");
+    }
+}
+
+class OverweightException extends Exception
+{
+    private final int currentWeight;
+    private final int maxWeight;
+
+    public OverweightException(int currentWeight, int maxWeight)
+    {
+        super ("Greutatea avionului este " + currentWeight + " t, depasind limita de " + maxWeight + "t.");
+        this.currentWeight = currentWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    public int getCurrentWeight()
+    {
+        return currentWeight;
+    }
+
+    public int getMaxWeight()
+    {
+        return maxWeight;
+    }
+}
+
+class AircraftSystemsException extends Exception
+{
+    public AircraftSystemsException()
+    {
+        super ("Cel putin unul dintre sistemele avionului (AP, hidraulice, motoare) nu functioneaza in parametri normali.");
+    }
+}
+
+class WeatherException extends Exception
+{
+    public WeatherException()
+    {
+        super ("Conditiile meteorologice sunt neprielnice zborului.");
+    }
+}
+
+class LowTirePressureException extends Exception
+{
+    private final int pressure;
+
+    public LowTirePressureException(int pressure)
+    {
+        super ("Presiunea in pneuri este " + pressure + "psi.");
+        this.pressure = pressure;
+    }
+
+    public int getPressure()
+    {
+        return pressure;
+    }
+}
+
+class FPLNotFilledException extends Exception
+{
+    public FPLNotFilledException()
+    {
+        super ("Nu s-a depus un plan de zbor.");
+    }
+}
+
+class ATCNotClearedException extends Exception
+{
+    public ATCNotClearedException()
+    {
+        super ("Nu s-a primit autorizatia de zbor ATC.");
+    }
+}
+
+class Avion
+{
+    private boolean     pilotPresent;
+    private boolean     doorsClosed;
+    private int         fuelLevel;
+    private int         totalWeight;
+    private boolean     systemsOK;
+    private int         tirePressure;
+    private boolean     weatherChecked;
+    private boolean     FPLFilled;
+    private boolean     ATCCleared;
+}
+
+public class PregatireZbor {
+    
+}
