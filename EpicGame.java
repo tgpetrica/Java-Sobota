887e8b476997291abc1606457d5be0a9f5fd9c99
Teodor <teodor.petrica@rovacc.ro>
2025-09-27 13:42:23 +0300

    add L37b.2

--- EpicGame.java
+++ EpicGame.java
@@ -82,8 +82,116 @@ class BigEnemy extends BaseEnemy {
     }
 }
 
+class Hero {
+    private int life = 100;
+    private int points = 0;
+    private final Weapon weapon;
+
+    public Hero(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public int getLife() {
+        return life;
+    }
+
+    public boolean isAlive() {
+        return life > 0;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void decreaseLife(int amount) {
+        life = Math.max(0, life - Math.max(0, amount));
+        if (life == 0) System.out.println("GAME OVER!");
+    }
+
+    public void addPoints(int pts) {
+        points += pts;
+    }
+
+    public void fight(Enemy enemy) {
+        if (isAlive()) {
+            if (enemy.isAlive()) {
+                int dmg = weapon.getDamage();
+                if (enemy instanceof BaseEnemy) {
+                    ((BaseEnemy) enemy).decreaseLife(dmg);
+                }
+                System.out.printf("HERO hits %s for %d dmg. %s life=%d%n",
+                                enemy.getName(), dmg, enemy.getName(), enemy.getLife());
+                enemy.fight(this);
+            } else {
+                System.out.printf("%s is already defeated.%n", enemy.getName());
+            }
+        } else {
+            // eroul a murit
+            System.out.println("GAME OVER!");
+            return;
+        }
+    }
+
+    public void heal() {
+        life = Math.min (100, life + 30);
+        System.out.printf("HERO heals. Life=%d%n", life);
+    }
+}
+
+class WeaponBuilder {
+    private int damage = 0;
+
+    public WeaponBuilder addBlade(int dmg) {
+        damage += dmg;
+        return this;
+    }
+
+    public WeaponBuilder addBuff(int dmg) {
+        damage += dmg;
+        return this;
+    }
+
+    public WeaponBuilder addHandle(int dmg) {
+        damage += dmg;
+        return this;
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+
+    public Weapon build(){
+        return new Weapon(this);
+    }
+}
+
+class Weapon {
+    private final int damage;
+
+    Weapon(WeaponBuilder builder) {
+        this.damage = builder.getDamage();
+    }
+
+    public int getDamage(){
+        return damage;
+    }
+}
+
 public class EpicGame {
     public static void main(String[] args) {
+        Weapon w = new WeaponBuilder()
+            .addBlade(25)
+            .addBuff(10)
+            .addHandle(5)
+            .build();
         
+        Hero hero = new Hero(w);
+
+        Enemy Echo = EnemyFactory.get("small");
+        Enemy Kilo = EnemyFactory.get("medium");
+
+        hero.fight(Echo);
+        hero.heal();
+        hero.fight(Kilo);
     }
 }
